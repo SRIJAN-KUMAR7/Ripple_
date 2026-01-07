c27f8f0d3ee67edda3db48da854d9cab9c59e9be
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2026-01-07 23:16:18 +0530

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import path from "path";
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import { connectDB } from "./lib/db.js";
+
 
 
 dotenv.config();
@@ -15,6 +17,8 @@ const PORT=process.env.PORT||3000;
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+app.use(express.json());//req.body
 
 //ready for deployment with only backend deployment 
 if(process.env.NODE_ENV=="production"){
@@ -28,4 +32,5 @@ if(process.env.NODE_ENV=="production"){
 
 app.listen(PORT,()=>{
     console.log("Server is running on port:"+ PORT);
+    connectDB();
 });

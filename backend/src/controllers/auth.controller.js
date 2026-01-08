81f67fb3ec690ecef28506b0c38a988a3b60669c
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2026-01-08 23:51:15 +0530

    signup endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,19 @@
 export const signup=async(req,res)=>{
-    res.send("Signup endpoint");
+    const {fullName,email,password}=req.body;
+    try{
+        if(!fullName||!email||!password){
+            return res.status(400).json({message:"All fields are required"});    
+        }
+        if(password.length<6){ return res.status(400).json({message:"Password must be at least 6 characters"});    }
+         //validating emails using regex
+        
+         const emailRegex=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+         if(!emailRegex.test(email)){
+            return res.status(400).json({message:"Invalid email format"});    
+         }
+
+         
+    }catch(err){
+
+    }
 }

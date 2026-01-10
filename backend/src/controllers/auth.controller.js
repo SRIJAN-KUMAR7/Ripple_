1dd1efc1690ed8e245a3839957a53bf6ae3d2e3e
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2026-01-10 10:38:23 +0530

    signup controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,7 @@
+import bcrypt from "bcryptjs"
+import User from "../models/User.js";
+import { generateToken } from "../lib/utils.js";
+
 export const signup=async(req,res)=>{
     const {fullName,email,password}=req.body;
     try{
@@ -11,9 +15,35 @@ export const signup=async(req,res)=>{
          if(!emailRegex.test(email)){
             return res.status(400).json({message:"Invalid email format"});    
          }
+     const user=await User.findOne({email:email});
+     if(user){return res.status(400).json({message:"Email already exists"})}
+     
+     //1234565--->#^&(bhdbfk&^$#$) gibberish 
+     const salt=await bcrypt.genSalt(10)
+     const hashedPassword=await bcrypt.hash(password,salt);
 
-         
+     const newUser= new User({
+        fullName,
+        email,
+        password:hashedPassword
+     })
+     if(newUser){
+      generateToken(newUser._id,res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id:newUser._id,
+        fullName:newUser.fullName,
+        email:newUser.email,
+        profilePic:newUser.profilePic,
+      })
+     }else{
+        res.status(400).json({message:"Invalid User data"});
+     }
+
+
     }catch(err){
-
+     console.log("Error in signup conteroller:",err);
+     res.status(500).json({message:"Internal server error"});
     }
 }

edbd42e68325cb176df2bf5369ea2d6aaf389b83
Srijan Kumar <srijankumar77777@gmail.com>
2026-01-18 11:36:03 +0530

    Welcome email functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs"
 import User from "../models/User.js";
 import { generateToken } from "../lib/utils.js";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 export const signup=async(req,res)=>{
     const {fullName,email,password}=req.body;
@@ -40,7 +41,13 @@ export const signup=async(req,res)=>{
         fullName:newUser.fullName,
         email:newUser.email,
         profilePic:newUser.profilePic,
-      })
+      });
+      try {
+        await sendWelcomeEmail(savedUser.email,savedUser.fullName,process.env.CLIENT_URL);
+        
+      } catch (error) {
+        console.log("Failed to send Welcome email",error)
+      }
      }else{
         res.status(400).json({message:"Invalid User data"});
      }

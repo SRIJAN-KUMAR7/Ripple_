42d5d93f3d588df9fb7af8152dee53ced17840ae
Srijan Kumar <srijankumar77777@gmail.com>
2026-01-18 11:36:26 +0530

    Welcome email handler

---
+++ backend/src/emails/emailHandlers.js
@@ -0,0 +1,17 @@
+import { resendClient,sender } from "../lib/resend.js";
+import {createWelcomeEmailTemplate} from "../emails/emailTemplates.js"
+
+export const sendWelcomeEmail=async(email,name,clientURL)=>{
+   const{data,error}=await resendClient.emails.send({
+    from:`${sender.name} <${sender.email}>`,
+    to :email,
+    subject: "Welcome to Ripple!",
+    html: createWelcomeEmailTemplate(name,clientURL)
+   });
+
+   if(error){
+    console.log("Error Sending welcome email",error);
+    throw new Error("Failed to send welcome email");
+   }
+    console.log("Welcome email send successfully",data);
+};

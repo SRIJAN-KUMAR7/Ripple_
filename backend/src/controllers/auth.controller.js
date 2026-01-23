61a75ccef4456d35cdb1e816e8b6fc11605464e0
Srijan Kumar <srijankumar77777@gmail.com>
2026-01-24 00:24:25 +0530

    login and logout http requests

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -59,3 +59,38 @@ export const signup=async(req,res)=>{
      res.status(500).json({message:"Internal server error"});
     }
 }
+
+
+export const login=async(req,res)=>{
+const{email,password}=req.body
+
+try {
+  const user=await User.findOne({email:email});
+  if(!user){
+    return res.status(400).json({message:"Invalid Credentials"});
+  }//never tell the client which one is incorrect email or pass
+
+  const isPasswordCorrect=await bcrypt.compare(password,user.password);
+  if(!isPasswordCorrect){
+       return res.status(400).json({message:"Invalid Credentials"});
+  }
+
+  generateToken(user._id,res);
+  res.status(200).json({
+    _id:user._id,
+    fullName:user.fullName,
+    email:user.email,
+    profilePic:user.profilePic,
+  })
+} catch (error) {
+  console.error("Erron in Login Controller ");
+  return res.status(500).json({message:"Internal server error!"})
+}
+
+}
+
+
+export const logout=async(_,res)=>{
+  res.cookie("jwt","",{maxAge:0});
+  res.status(200).json({message:"Logged Out successfully"})
+}

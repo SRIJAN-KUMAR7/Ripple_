454f54c1718fd552b8d62e345b59f9628f754b31
Srijan Kumar <srijankumar77777@gmail.com>
2026-02-09 19:29:13 +0530

    controller and schema

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,62 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+
+export const getAllContacts=async(req,res)=>{
+
+    try {
+        const loggedInUsers=req.user._id;
+        const filteredUsers=await User.find({_id: {$ne: loggedInUsers}}).select(-"password")
+
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.log("Error in getAllContacts:",error);
+        res.status(500).json({message:"Server error!"})
+    }
+};
+
+export const getMessagesByUserId=async(req,res)=>{
+    try {
+        const myId=req.user._id;
+        const {id : UserToChat}=req.params;
+
+        const messages=await Message.find({
+            $or:[
+                {senderId:myId,receiverId:UserToChat},
+                {senderId:UserToChat,receiverId:myId},
+            ],
+        });
+        res.status(200).json(messages);
+    } catch (error) {
+       console.log("Error in getMessages Controller :",error.message); 
+    }
+};
+
+export const sendMessage=async(req,res)=>{
+    try {
+       const {text,image}=req.body;
+       const {id :receiverId}=req.params;
+
+       const senderId=req.user._id;
+
+       let imageUrl;
+       if(image){
+        //upload base64 to cloudinary
+        const uploadResponse=await cloudinary.uploader.upload(image);
+        imageUrl=uploadResponse.secure_url;
+       }
+       const newMessage= new Message({
+        senderId,
+        receiverId,
+        text,
+        image:imageUrl,
+       })
+       await newMessage.save();
+      
+       //todo : send message in real time if user is online -socket.io 
+       res.status(201).json(newMessage)
+
+    } catch (error) {
+        console.log("Error in SendMessage controller:",error.message);
+    }
+};

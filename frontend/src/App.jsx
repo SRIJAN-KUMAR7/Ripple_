43163f1d6355a7ac8821681173602e379ca372ab
Srijan Kumar <srijankumar77777@gmail.com>
2026-02-17 23:31:03 +0530

    new ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import ChatPage from './pages/ChatPage'
 import LoginPage from './pages/LoginPage'
@@ -6,10 +6,14 @@ import SignUpPage from './pages/SignUpPage'
 import { useAuthStore } from './store/useAuthStore'
 
 const App = () => {
-  const { authUser, login,isLoggedIn } = useAuthStore()
+const {checkAuth,isCheckingAuth,authUser}=useAuthStore();
 
-  console.log("authuser:", authUser)
-  console.log("isLoggedIn:", isLoggedIn)
+useEffect(()=>{
+  checkAuth();
+},[checkAuth]);
+console.log({authUser})
+
+
 
   return (
     <div className='min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden text-white'>
@@ -20,9 +24,8 @@ const App = () => {
       <div className="absolute -top-40 left-1/2 -translate-x-1/2 w-[600px] h-[600px] bg-white opacity-10 blur-[140px]" />
       <div className="absolute bottom-0 -right-32 w-[500px] h-[500px] bg-gradient-to-tr from-white/10 via-gray-300/10 to-transparent blur-[120px]" />
 
-      {/* Content wrapper with higher z-index */}
+
       <div className="relative z-10 w-full max-w-4xl">
-        <button onClick={login}>Test Button</button>
         <Routes>
           <Route path="/" element={<ChatPage />} />
           <Route path="/login" element={<LoginPage />} />

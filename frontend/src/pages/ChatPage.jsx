802441337b09b474dc41642d200062a36f0496e4
Srijan Kumar <srijankumar77777@gmail.com>
2026-04-16 17:29:51 +0530

    add page for chat

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -9,21 +9,23 @@ import ChatContainer from "../components/ChatContainer";
 import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
 
 function ChatPage() {
-  const { activeTab, selectedUser } = useChatStore();
+  const { activeTab, selectedUser, isSidebarCollapsed, toggleSidebar } = useChatStore();
 
   return (
     <div className="relative w-full h-screen bg-[#0f0f0f] flex overflow-hidden">
       {/* LEFT SIDE */}
-      <div className="w-80 bg-[#0f0f0f] flex flex-col border-r border-[#1f1f1f]">
-        < ProfileHeader />
-        <ActiveTabSwitch />
+      <div className={`transition-all duration-300 ease-in-out flex flex-col border-r border-[#1f1f1f] bg-[#0f0f0f] ${isSidebarCollapsed ? "w-20" : "w-80"}`}>
+        <div className="relative flex flex-col h-full overflow-hidden">
+          <ProfileHeader />
+          <ActiveTabSwitch />
 
-        <div className="flex-1 overflow-y-auto py-2 px-2 space-y-0.5 
-            [&::-webkit-scrollbar]:w-1.5
-            [&::-webkit-scrollbar-track]:transparent
-            [&::-webkit-scrollbar-thumb]:rounded-full
-            [&::-webkit-scrollbar-thumb]:bg-[#1f1f1f]">
-          {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+          <div className="flex-1 overflow-y-auto py-2 px-2 space-y-0.5 
+              [&::-webkit-scrollbar]:w-1.5
+              [&::-webkit-scrollbar-track]:transparent
+              [&::-webkit-scrollbar-thumb]:rounded-full
+              [&::-webkit-scrollbar-thumb]:bg-[#1f1f1f]">
+            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+          </div>
         </div>
       </div>
 
